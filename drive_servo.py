cbf7104c6d3030726ae031b58db34df4036b1f72
root <root@spacepi.(none)>
2013-04-08 20:10:09 +0000

    Servo driver script

--- drive_servo.py
+++ drive_servo.py
@@ -12,14 +12,13 @@ tiltStepSize = -2 # degree of change for each tilt update
 servoPanPosition = 224 # initial pan position
 servoTiltPosition = 160 # initial tilt position
 
-panGpioPin = 6  # servoblaster pin 2 : gpio pin 18
-tiltGpioPin = 7  # servoblaster pin 5 : gpio pin 23
+panGpioPin = 0  # servoblaster pin 2 : gpio pin 18
+tiltGpioPin = 1  # servoblaster pin 5 : gpio pin 23
 
 def moveServo(servo, angle):
     move(servo, angle)
     time.sleep(0.5)
     move(servo, 0)
-    time.sleep(0.5)
 
 def move(servo, angle):
     '''Moves the specified servo to the supplied angle.
@@ -34,18 +33,11 @@ def move(servo, angle):
     command = 'echo %s=%s > /dev/servoblaster' % (str(servo), str(angle))
     os.system(command)
 
-#    if (min_pwm <= angle <= max_pwm):
-#        command = 'echo %s=%s > /dev/servoblaster' % (str(servo), str(angle))
-#        os.system(command)
-#        #print command
-#    else:
-#        print "Servo angle must be an integer between %s and %s.\n" % (str(min_pwm), str(max_pwm))
-
 if __name__ == '__main__':
     print(sys.argv[1])
-    if (sys.argv[1] == "1"):
+    if (sys.argv[1] == "0"):
       print(sys.argv[2])
       moveServo(panGpioPin, int(sys.argv[2]))
 
-    elif (sys.argv[1] == "2"):
+    elif (sys.argv[1] == "1"):
       moveServo(tiltGpioPin, int(sys.argv[2]))
